d69cd240cd3e7391ca8b089b3c687419d3ddfa1b
DraperRiley <rileyldraper@gmail.com>
2023-10-23 22:21:58 -0500

    Further progress
    Stuffs

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,36 +1,61 @@
+org 0x7c00
 bits 16
 
 ; begin
-section .text
+	section .text
 	; start at entry
 	global _entry
-	extern _KernelMain
+	; extern _KernelMain
+
+_kernel_addr:
+	dw 0x8c00
 
 _entry:
 
 	; clear interrupts
 	cli
 	
-	; get ready
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
+	; TODO: enable A20 line
 
-	; stack
-	mov ss, ax
-	mov sp, 0x7C00
+	; set gdt
+	call set_gdt
+
+	; go protected mode
+	bits 32
+	; TODO: set protected mode
 
 	; enter kernel.c
-        call _KernelMain
+        ; call _KernelMain
+	call _kernel_addr
 
 	; should not end up here
-	jmp loop
+	jmp .loop
 
 .loop:
-	; oops
+	;oops
 	cli
 	hlt
 	jmp .loop
+
+GDT:
+set_gdt:
+	; configure gdt
+	xor eax, eax
+	mov ax, ds
+	shl eax, 4
+	add eax, GDT
+	mov [gdtr + 2], eax
+	mov eax, GDT_end
+	sub eax, GDT
+	mov [gdtr], ax
+	lgdt [gdtr]
+	ret
+
+gdtr:
+	dw 0
+	dw 0
+
+GDT_end:
 
 times 510 - ($-$$) db 0
 dw 0xAA55
